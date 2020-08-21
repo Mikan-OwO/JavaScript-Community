4f8c6a6afbf5975ec5ea209780c25c73b87e2a1d
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-21 16:04:52 +0900

    Update addFile.js

--- addFile.js
+++ addFile.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 $("#+btn").on("click", async function () {
-fs.open(`./users-page/${$("textarea[name='title']").val()}.html`, "<!DOCTYPE html>", (err) => {
+fs.open(`./users-page/${$("textarea[name='title']").val()}.html`, '<!DOCTYPE html><html lang="ja"></html>', (err) => {
   if(err) console.log(err);
 });
 });
